5d4763484db8641f505fcf311940218bc0f3f680
pkubioinformatics <1401111630@pku.edu.cn>
2020-04-17 16:28:14 +0800

    Update NanoReviser_train.py

--- NanoReviser_train.py
+++ NanoReviser_train.py
@@ -128,7 +128,7 @@ if __name__ == '__main__':
         logger = logger_config(log_path='./unitest/unitest_log.txt', logging_name='unitest')
         ar_args.epochs=2
         ar_args.read_counts=1
-        ar_args.window_size
+        ar_args.window_size=5
     try:
         start_time = time.time()
         try:
