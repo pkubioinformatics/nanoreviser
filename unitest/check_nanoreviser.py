bd1d82a6f4ac720ecd1b2ca6ab9507e1d5027c99
pkubioinformatics <1401111630@pku.edu.cn>
2020-04-17 15:04:14 +0800

    new submit

---
+++ unitest/check_nanoreviser.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+ @File: nanoreviser - check_nanoreviser
+ 
+ @Time: 2020/4/17 2:56 PM
+ 
+ @Author: lotuswang
+ 
+ 
+"""
+
+import os
+import shutil
+
+if __name__ == '__main__':
+    log_fn='./unitest_log.txt'
+    with open(log_fn, 'r') as lg:
+        result_list = lg.readlines()
+    assert len(result_list)==3
+    for result in result_list:
+        tags=result.split(' - ')
+        tag1 = tags[2]
+        tag2 = tags[-1].split(',')[0]
+        assert (tag1=='INFO' and tag2=='Congratulations')
+    os.remove(log_fn)
+    if os.path.exists('../unintest'):
+        shutil.rmtree('../unintest')
+    if os.path.exists('../unitest_training_results/'):
+        shutil.rmtree('../unitest_training_results')
+    print("Congratulations, please have fun with NanoReviser :)")
+
